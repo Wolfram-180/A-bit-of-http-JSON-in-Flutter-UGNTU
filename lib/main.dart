9f7f32bf1cc013ee2f646d8892afc85df8ef9f44
wolfram-180 <sergei.ulvis@gmail.com>
2021-12-27 18:40:47 +0300

    task done

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,11 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-Future<Album> fetchAlbum() async {
+Future<Album> fetchAlbum(int parse) async {
   final response = await http
-      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
-
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/$parse'));
   if (response.statusCode == 200) {
-    return Album.fromJson(jsonDecode(response.body));
+    return Album.fromJson(json.decode(response.body));
   } else {
     throw Exception('Failed to load album');
   }
@@ -35,49 +34,131 @@ class Album {
   }
 }
 
-void main() => runApp(const MyApp());
+Future<Post> fetchPost(int parse) async {
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/posts/$parse'));
+  if (response.statusCode == 200) {
+    return Post.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed to load Post');
+  }
+}
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.body,
+  });
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+}
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  late Future<Album> futureAlbum;
+  final _controller = TextEditingController();
+
+  late Future<Post> futurePost;
 
   @override
   void initState() {
     super.initState();
-    futureAlbum = fetchAlbum();
+    futurePost = fetchPost(1);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    const String _appTitle = 'Fetch Posts Data Example';
     return MaterialApp(
-      title: 'Fetch Data Example',
+      title: _appTitle,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Fetch Data Example'),
+          title: const Text(_appTitle),
         ),
-        body: Center(
-          child: FutureBuilder<Album>(
-            future: futureAlbum,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(snapshot.data!.title);
-              } else if (snapshot.hasError) {
-                return Text('${snapshot.error}');
-              }
-
-              // By default, show a loading spinner.
-              return const CircularProgressIndicator();
-            },
-          ),
+        body: ListView(
+          children: [
+            Stack(
+              alignment: const Alignment(1.0, 1.0),
+              children: [
+                TextField(
+                  controller: _controller,
+                  decoration:
+                      const InputDecoration(hintText: 'Enter Post ID...'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    _controller.clear();
+                  },
+                  child: const Icon(Icons.clear),
+                ),
+              ],
+            ),
+            ElevatedButton(
+              onPressed: () {
+                futurePost = fetchPost(int.parse(_controller.text));
+                if (_controller.text.isNotEmpty) {
+                  setState(() {
+                    _controller.clear();
+                  });
+                  FocusScope.of(context).requestFocus(FocusNode());
+                }
+              },
+              child: const Text('Get Post', style: TextStyle(fontSize: 20)),
+            ),
+            Column(
+              children: [
+                Center(
+                  child: FutureBuilder<Post>(
+                    future: futurePost,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return Column(
+                          children: [
+                            const Text('Post tile:'),
+                            Text(snapshot.data!.title),
+                            const Divider(),
+                            const Text('Post body:'),
+                            Text(snapshot.data!.body),
+                          ],
+                        );
+                      } else if (snapshot.hasError) {
+                        return Text('${snapshot.error}');
+                      }
+                      return const CircularProgressIndicator();
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
