d690043bc3624e0f4a81d8017e1a39081ae43f59
wolfram-180 <sergei.ulvis@gmail.com>
2021-12-27 17:46:25 +0300

    http, JSON example - initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ugntu_flutter_course_case_3_2
+
+http, JSON
---
+++ android/app/src/main/kotlin/com/example/ugntu_flutter_course_case_3_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ugntu_flutter_course_case_3_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
